9f5930a7fc85fe2c2245da32eda331ffde964cb4
farivar <farivaramiri@gmail.com>
2023-07-30 10:39:35 -0700

    fix some bugs2

--- src/index.js
+++ src/index.js
@@ -279,6 +279,7 @@ document.getElementById('submit-btn').addEventListener('click', function() {
 
 updateVisualization('USD', '');
 
+
 function updateVisualization(baseCurrency, date) {
   let apiUrl = `${backendServerUrl}/?url=https://v6.exchangerate-api.com/v6/5a18964394c03fc2da032a8c/${date ? 'history' : 'latest'}/${baseCurrency}${date ? '/' + date : ''}`;
 
@@ -288,48 +289,26 @@ function updateVisualization(baseCurrency, date) {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        let rates = data.conversion_rates || data.historical;;
-        // Define a color scale using d3.scaleOrdinal()
-        // The range of colors used is d3's built-in 'schemeCategory10', but you can adjust this as desired
-        let color = d3.scaleOrdinal()
-        .domain(countries.map(country => country.properties.name))  // Use the country names as the domain
-        .range(d3.schemeCategory10);  // Use the 10-category color scheme
-        
-        color.domain(d3.extent(Object.values(rates)));  // Set the color domain based on rates
-        countries.forEach(country => {
-          let currencyCode = countryToCurrency[country.properties.name];
-          if (currencyCode) {
-            country.rate = rates[currencyCode];
-          } else {
-            console.warn(`No currency mapping found for country: ${country.properties.name}`);
-          }
-        });
+          var rates = data.conversion_rates;
 
-        // Clear existing visualization
-        svg.selectAll("*").remove();
-
-        // Draw world map
-        svg.selectAll(".country")
-          .data(countries)
-          .enter().append("path")
-          .attr("class", "country")
-          .attr("d", path)
-          .attr("fill", function(d) { 
-            return color(d.properties.name);  // Use the color scale to assign a unique color to each country
-          })
-          .on("mouseover", function (event, d) {
-            tooltip.transition()
-              .duration(200)
-              .style("opacity", 0.9);
-            tooltip.html(`${d.properties.name}: ${d.rate || "No data"}`)
-              .style("left", (event.pageX) + "px")
-              .style("top", (event.pageY - 28) + "px");
-          })
-          .on("mouseout", function (d) {
-            tooltip.transition()
-              .duration(500)
-              .style("opacity", 0);
+          let color = d3.scaleOrdinal()
+                  .domain(countries.map(country => country.properties.name))
+                  .range(d3.schemeCategory10);
+          countries.forEach(country => {
+              let currencyCode = countryToCurrency[country.properties.name];
+              if (currencyCode) {
+                  country.rate = rates[currencyCode];
+              } else {
+                  console.warn(`No currency mapping found for country: ${country.properties.name}`);
+              }
           });
+
+          // Update world map
+          svg.selectAll(".country")
+              .data(countries)
+              .attr("fill", function(d) { 
+                  return d.rate ? color(d.rate) : 'gray';  // Use the rate to color the country
+              });
       })
       .catch(error => console.log('Error:', error));
   });
@@ -337,123 +316,107 @@ function updateVisualization(baseCurrency, date) {
 
 
 
-// // Create a color scale function
-// function colorScale(percentChange) {
-//   let r, g, b = 0;
 
-//   if (percentChange < 0) {
-//       r = 255;
-//       g = Math.round(255 * (percentChange / 100 + 1));
-//   } else {
-//       g = 255;
-//       r = Math.round(255 * ((100 - percentChange) / 100));
-//   }
+// Create a color scale function
+function colorScale(percentChange) {
+  let r, g, b = 0;
+  console.log(percentChange);
+  if (percentChange < 0) {
+    r = 255;
+    g = Math.round(255 * (percentChange * 10 + 1));
+  } else {
+    g = 255;
+    r = Math.round(255 * ((100 - percentChange) * 10));
+  }
 
-//   return "rgb(" + r + "," + g + "," + b + ")";
-// }
+  return "rgb(" + r + "," + g + "," + b + ")";
+}
 
-// // Function to fetch exchange rates
-// async function fetchExchangeRates(date) {
-//   // API endpoint - replace with actual endpoint
-//   let url = "https://api.exchangeratesapi.io/" + date;
 
-//   let response = await fetch(url);
-//   let data = await response.json();
+// Function to calculate percent change
 
-//   return data.rates;
-// }
+function calculatePercentChange(current, previous) {
+  return ((current - previous) / previous) * 100;
+}
 
-// // Function to calculate percent change
-// function calculatePercentChange(current, previous) {
-//   return ((current - previous) / previous) * 100;
-// }
 
-// // Function to update map
-// async function updateMap(baseDate) {
-//   // Fetch exchange rates
-//   let currentRates = await fetchExchangeRates(baseDate);
-//   let previousRates = await fetchExchangeRates(new Date(baseDate.getTime() - 24 * 60 * 60 * 1000)); // day before
 
-//   // For each country, calculate the percent change and update color on the map
-//   for (let country in currentRates) {
-//       let percentChange = calculatePercentChange(currentRates[country], previousRates[country]);
 
-//       // Get country element on the map - replace with actual method
-//       let countryElement = document.getElementById(country);
+// Modify fetchExchangeRates to include baseCurrency
+async function fetchExchangeRates(baseCurrency, date) {
+  let formattedDate = date ? new Date(date).toISOString().split('T')[0].replace(/-/g, '/') : '';
+  let url = `${backendServerUrl}/?url=https://v6.exchangerate-api.com/v6/5a18964394c03fc2da032a8c/history/${baseCurrency}${formattedDate ? '/' + formattedDate : ''}`;
+  let response = await fetch(url);
+  let data = await response.json();
+  
+  return data.conversion_rates;
+}
 
-//       // Update color
-//       countryElement.style.fill = colorScale(percentChange);
-//   }
-// }
+// Modify updateMap to include baseCurrency
+async function updateMap(baseCurrency, baseDate) {
+  let currentRates = await fetchExchangeRates(baseCurrency, baseDate);
+  let previousRates = await fetchExchangeRates(baseCurrency, new Date(baseDate.getTime() - 24 * 60 * 60 * 1000)); // day before
 
-// // Event listener for date selection
-// document.getElementById('date-picker').addEventListener('change', function() {
-//   let baseDate = new Date(this.value);
-//   updateMap(baseDate);
+  d3.json("https://unpkg.com/world-atlas@2/countries-110m.json").then(function(world) {
+    var countries = topojson.feature(world, world.objects.countries).features;
+
+    countries.forEach(country => {
+        let currencyCode = countryToCurrency[country.properties.name];
+        if (currencyCode && currentRates[currencyCode] && previousRates[currencyCode]) {
+            let percentChange = calculatePercentChange(currentRates[currencyCode], previousRates[currencyCode]);
+            country.percentChange = percentChange;
+        } else {
+            console.warn(`No currency mapping or no rate data found for country: ${country.properties.name}`);
+        }
+    });
+
+    // Create a tooltip
+    var tooltip = d3.select("body")
+      .append("div")
+      .style("position", "absolute")
+      .style("z-index", "10")
+      .style("visibility", "hidden")
+      .style("background", "#ffffff")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px")
+      .text("a simple tooltip");
+
+    // Update world map
+    svg.selectAll(".country")
+        .data(countries)
+        .attr("fill", function(d) { 
+            return d.percentChange ? colorScale(d.percentChange) : 'gray';
+        })
+        .on("mouseover", function(event, d) {
+            tooltip.text(`${d.properties.name}: ${d.percentChange ? d.percentChange.toFixed(2) + '%' : 'N/A'}`);
+            return tooltip.style("visibility", "visible");
+        })
+        .on("mousemove", function(event) {
+            return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+        })
+        .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+  });
+}
+
+document.getElementById('date-picker').addEventListener('change', function() {
+  let baseDate = new Date(this.value);
+  let currencyB = document.getElementById('currency-base').value;
+  updateMap(currencyB, baseDate); 
+});
+
+// document.getElementById('change').addEventListener('click', function() {
+//   let currencyB = document.getElementById('currency-base').value;
+//   let baseDate = document.getElementById('date-picker').value;
+
+//   // Convert date to required format: 'YEAR/MONTH/DAY'
+//   let formattedDate = baseDate ? new Date(baseDate).toISOString().split('T')[0].replace(/-/g, '/') : '';
+
+//   updateMap(currencyB, formattedDate);
 // });
 
-
-// async function fetchExchangeRates(date) {
-
-//   try {
-
-//     let formattedDate = date.toISOString().split('T')[0].replace(/-/g, '/');
-    
-//     let apiUrl = `${backendServerUrl}/?url=https://v6.exchangerate-api.com/v6/5a18964394c03fc2da032a8c/${formattedDate}`;
-
-//     const response = await fetch(apiUrl);
-
-//     const data = await response.json();
-
-//     return data.conversion_rates;
-
-//   } catch (error) {
-
-//     console.log('Error fetching exchange rates:', error);
-    
-//     return {}; // Return an empty object to handle missing data
-
-//   }
-
-// }
-
-// // Add click handler for change button
-// const changeButton = document.getElementById('change');
-// changeButton.addEventListener('click', showChange);
-
-// // Calculate percentage change 
-// function calculatePercentChange(current, previous) {
-//   return ((current - previous) / previous) * 100;
-// }
-
-// // Get color based on percent change
-// function getColor(percentChange) {
-//   if(percentChange > 0) return 'green';
-//   if(percentChange < 0) return 'red';
-//   return 'white';
-// }
-
-// // Show change from previous day 
-// async function showChange() {
-
-//   // Get today and yesterday's rates
-//   const todayRates = await fetchExchangeRates(new Date()); 
-//   const yesterdayRates = await fetchExchangeRates(new Date(Date.now() - 86400000));
-
-//   // Update map
-//   svg.selectAll('.country')
-//     .style('fill', d => {
-//       const name = d.properties.name;
-//       const todayRate = todayRates[name];
-//       const yesterdayRate = yesterdayRates[name];
-//       if(todayRate && yesterdayRate) {
-//         const change = calculatePercentChange(todayRate, yesterdayRate);
-//         return getColor(change);  
-//       }
-//       return 'gray';
-//     });
-
-// }
+// updateMap('USD', '');
 
 
 
