657e5242a90e706eac7d5b06571f57cb613d3615
farivar <farivaramiri@gmail.com>
2023-08-01 19:01:40 -0700

    ss

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -3,7 +3,7 @@ class Music {
       this.currentSongIndex = 0;
       this.songs = [
         new Audio('./src/songs/80s 100 Hits - Volume 2 CD 1 TRACK 4 (320).mp3'),
-        new Audio('./src/songs/Blackstreet, Dr. Dre, Queen Pen - No Diggity.mp3'),
+        // new Audio('./src/songs/Blackstreet, Dr. Dre, Queen Pen - No Diggity.mp3'),
         new Audio('./src/songs/Earth, Wind & Fire - Lets Groove.mp3'),
         new Audio('./src/songs/Grover_Washington,_Jr_,_Bill_Withers_Just_the_Two_of_Us_feat_Bill.mp3'),
         new Audio('./src/songs/Huey Lewis And The News - The Power Of Love.mp3'),
